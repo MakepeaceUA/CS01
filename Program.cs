6625cf5bf47fb4ab30bce9d52057bac6f6829164
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2025-01-19 17:02:42 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int MessageBox(IntPtr hWnd, string F_Text, string L_Text,int type);
+        static void Main(string[] args)
+        {
+            string f_name = "Арсений";
+            string l_name = "Севастьянов";
+            string age = "18";
+
+            MessageBox(IntPtr.Zero, f_name, "Имя",0);
+            MessageBox(IntPtr.Zero, l_name, "Фамилия",0);
+            MessageBox(IntPtr.Zero, age, "Возраст",0);
+        }
+    }
+}
